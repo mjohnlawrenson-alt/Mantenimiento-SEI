a47eb31ab20f6f25d59daa9869831c429ee4a3e9
mjohnlawrenson-alt <m.johnlawrenson@gmail.com>
2026-02-04 19:54:26 +0100

    Update misllaves.ts

--- src/misllaves.ts
+++ src/misllaves.ts
@@ -1,15 +1,12 @@
-// @ts-nocheck
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// ... imports ...
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3NIor3T-VbUC9SfzSgYP7DxirH8leeSo", 
-  authDomain: "sei-europa-mantenimiento.firebaseapp.com",
-  projectId: "sei-europa-mantenimiento",
-  storageBucket: "sei-europa-mantenimiento.appspot.com",
-  messagingSenderId: "931985332296", 
-  appId: "1:931985332296:web:60037592440938228105e4"
+  apiKey: "AIzaSyD...",
+  authDomain: "sei-europa...",
+  projectId: "sei-europa...",
+  storageBucket: "sei-europa-mantenimiento.firebasestorage.app", // <--- ¡ESTA ES LA CLAVE QUE FALTA!
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// ... export ...

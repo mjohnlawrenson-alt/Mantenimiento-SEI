ff731bf32bbda96838bb7540e95581411d2485ed
mjohnlawrenson-alt <m.johnlawrenson@gmail.com>
2026-02-17 16:02:20 +0100

    Update misllaves.ts

--- src/misllaves.ts
+++ src/misllaves.ts
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // <--- ¡NUEVO!
 
 // --- TUS LLAVES DE FIREBASE ---
-// Copia los valores de tu consola de Firebase y ponlos aquí dentro de las comillas.
-
 const firebaseConfig = {
   apiKey: "AIzaSyA3NIor3T-VbUC9SfzSgYP7DxirH8leeSo",
   authDomain: "sei-europa-mantenimiento.firebaseapp.com",
   projectId: "sei-europa-mantenimiento",
-  storageBucket: "sei-europa-mantenimiento.firebasestorage.app", 
+  storageBucket: "sei-europa-mantenimiento.firebasestorage.app",
   messagingSenderId: "1001646514383",
   appId: "1:1001646514383:web:c697e9cf7881dcfc5e17ba"
 };
 
-// --- INICIALIZACIÓN (NO TOCAR) ---
+// --- INICIALIZACIÓN ---
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app); // <--- ¡NUEVO! (Esto faltaba)
